dc30c11f82451578ded5dd31ef0e72cba814967d
MCOfficer <mcofficer@gmx.de>
2021-05-25 16:56:42 +0200

    Initial commit

---
+++ src/bw_cli.rs
@@ -0,0 +1,120 @@
+use anyhow::{Context, Result};
+use chrono::{DateTime, Utc};
+use duct::cmd;
+use lazy_static::lazy_static;
+use log::error;
+use log::info;
+use parking_lot::RwLock;
+use serde::Deserialize;
+use std::ffi::{OsStr, OsString};
+use std::io::stdin;
+
+lazy_static! {
+    static ref SESSION_KEY: RwLock<Option<String>> = RwLock::new(None);
+}
+
+pub fn login() -> Result<()> {
+    let status = status().context("Failed to get status")?;
+    dbg!(&status);
+
+    let (email, password) = prompt_credentials(&status)?;
+
+    let session = if VaultStatus::UNAUTHENTICATED == status.vault_status {
+        info!("Logging in...");
+        call_bw(vec!["login", "--raw", email.trim(), &password])?
+    } else {
+        info!("Already logged in, unlocking vault...");
+        call_bw(vec!["unlock", "--raw", &password])?
+    };
+
+    info!("Acquired session key");
+
+    let mut guard = SESSION_KEY.write();
+    *guard = Some(session);
+    drop(guard);
+
+    Ok(())
+}
+
+fn prompt_credentials(status: &Status) -> Result<(String, String)> {
+    let email = match status.user_email.clone() {
+        None => {
+            println!("E-Mail:");
+            let mut email = String::new();
+            stdin().read_line(&mut email)?;
+            email
+        }
+        Some(email) => {
+            info!("Using E-Mail from Bitwarden: {}", email);
+            info!("If you want to use a different E-Mail, please use 'bw logout'");
+            email
+        }
+    };
+
+    let password =
+        rpassword::prompt_password_stdout("Password:\n").context("Failed to read password")?;
+
+    Ok((email.clone(), password))
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+enum VaultStatus {
+    UNLOCKED,
+    LOCKED,
+    UNAUTHENTICATED,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Status {
+    server_url: String,
+    last_sync: Option<DateTime<Utc>>,
+    user_email: Option<String>,
+    user_id: Option<String>,
+    #[serde(rename = "status")]
+    vault_status: VaultStatus,
+}
+
+pub fn status() -> Result<Status> {
+    let stdout = call_bw(vec!["status"])?;
+    let status: Status = serde_json::from_str(&stdout).context("Failed to serialize status")?;
+    Ok(status)
+}
+
+pub fn call_bw<A>(args: Vec<A>) -> Result<String>
+where
+    A: Into<OsString> + AsRef<OsStr>,
+{
+    let cmd = cmd("bw", &args)
+        .stderr_capture()
+        .stdout_capture()
+        .env(
+            "BW_SESSION",
+            SESSION_KEY.read().as_ref().unwrap_or(&"".to_string()), // Passing an empty string will make bitwarden ignore it
+        )
+        .unchecked();
+
+    let output = cmd.run().context("Error running command")?;
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    if !output.status.success() {
+        let subcmd: OsString = args.get(0).unwrap().into();
+        let status_code = output
+            .status
+            .code()
+            .map_or("(No exit code)".into(), |u| u.to_string());
+        error!(
+            "'bw {}' returned non-zero exit code {}",
+            subcmd.to_string_lossy(),
+            status_code
+        );
+        error!("STDERR WAS:\n{}", stderr);
+        error!("STDOUT WAS:\n{}", stdout);
+
+        return Err(anyhow::format_err!("Error calling bitwarden cli"));
+    }
+
+    Ok(stdout.into())
+}
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+mod bw_cli;
+
+use chrono;
+use fern;
+use log::LevelFilter;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn setup_logger() {
+    fern::Dispatch::new()
+        .level(LevelFilter::Debug)
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}] {} {}",
+                chrono::Local::now().format("[%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(std::io::stdout())
+        .chain(fern::log_file("bitwarden-autotype.log").unwrap())
+        .apply()
+        .unwrap();
+}
+
+fn register_hotkeys() {
+    // TODO
+}
+
+fn main() {
+    setup_logger();
+    bw_cli::login().unwrap();
+    register_hotkeys();
+    loop {
+        sleep(Duration::from_millis(50));
+    }
+}
