b7c8634cd49af2efa684848f604784052efef596
MCOfficer <mcofficer@gmx.de>
2021-06-06 18:32:41 +0200

    Make use of disabled items in the tray menu

--- src/tray.rs
+++ src/tray.rs
@@ -1,6 +1,6 @@
 use log::{info, warn};
 use std::process::exit;
-use trayicon::{Icon, MenuBuilder, TrayIconBuilder};
+use trayicon::{Icon, MenuBuilder, MenuItem, TrayIconBuilder};
 use winapi::_core::mem::MaybeUninit;
 use winapi::um::winuser;
 
@@ -9,6 +9,15 @@ enum Events {
     Exit,
     Dummy,
     SyncNow,
+}
+
+fn info_item(name: &str) -> MenuItem<Events> {
+    MenuItem::Item {
+        id: Events::Dummy,
+        name: name.to_string(),
+        disabled: true,
+        icon: None,
+    }
 }
 
 pub fn main(username: String) {
@@ -22,10 +31,10 @@ pub fn main(username: String) {
         .tooltip("Bitwarden Autotype")
         .menu(
             MenuBuilder::new()
-                .item("Bitwarden Autotype is running", Events::Dummy)
+                .with(info_item("Bitwarden Autotype is running"))
                 .separator()
-                .item(&format!("Logged in as {}", username), Events::Dummy)
-                .item("Hotkey: Ctrl-Alt-A", Events::Dummy)
+                .with(info_item(&format!("Logged in as {}", username)))
+                .with(info_item("Hotkey: Ctrl-Alt-A"))
                 .separator()
                 .item("Sync Vault", Events::SyncNow)
                 .separator()
